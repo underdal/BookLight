caa99adf546c611c3cc266407b66ba839eabce39
hakonk <hakonknutzen@Hakons-MacBook-Pro.local>
2015-09-03 16:10:02 +0200

    Added a test method for saving an object with a callback for failure and success (see comments in code)

--- app/scripts/controllers/bookService.js
+++ app/scripts/controllers/bookService.js
@@ -19,6 +19,34 @@ bookLightApp.service('BookService',
 	    //return $resource(booksResource);
 	};
 
+	/** 
+	Creates new a new parse object of class TestBook. 
+	Find out how the object to be saved can be passed into this function
+	e.g. in parameter 'book' as shown below or maybe the parse object
+	can be retrieved from this.books via an index or key?
+	If not wanting to use parse, the POST or UPDATE calls can substitute the 
+	the testBook.save... call, while still using the same callback function with 
+	a desired input value corresponding to a success/failure.
+	*/
+	this.saveBook = function (book, callback) {
+
+
+
+		var TestBook = Parse.Object.extend("TestBook");
+		var testBook = new TestBook();
+
+		testBook.save(null, {
+			success: function ()
+			{
+				callback(true);
+			},
+			error : function ()
+			{
+				callback(false);
+			}
+		});
+	};
+
 	this.getBookData = function(query, callback){
 		//Get json from http books...
 		if( query && query.length > 0){
