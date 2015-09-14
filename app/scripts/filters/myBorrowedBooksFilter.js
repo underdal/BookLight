6a66cea3e106b911e08ef0cd31deb6b0bf0802a2
underdal <a.underdal@gmail.com>
2015-09-14 12:07:25 +0200

    Added filter my borrowed books

---
+++ app/scripts/filters/myBorrowedBooksFilter.js
@@ -0,0 +1,14 @@
+'use strict';
+bookLightApp.filter('myBorrowedBooksFilter', function(){
+	return function(books){
+		if(typeof books !== 'undefined'){
+			var myBooks = [];
+			for(var i=0; i< books.length; i++){
+				if(books[i].attributes.borrowedBy === 'Anlaug Underdal'){
+					myBooks.push(books[i]);
+				}
+			}
+			return myBooks;
+		}
+	}
+});
